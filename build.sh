27ae324c7aa6301432600738a0e0234859da51c4
Narayanan Krishnan <narayanan.k.1985@gmail.com>
2019-11-24 21:14:51 +0000

    modified build.sh docker-demo/misc/Jenkinsfile

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-rm -rf customapi-integration docker-compose email-notifications jenkins-slave jfrog-integration job-dsl node_modules pipeline_script
-rm -rf scripts slack-notifications sonarqube ssh-agent jenkins-docker
-cp -Rv docker-demo/* .
-rm -rf docker-demo
 npm install
---
+++ clean.sh
@@ -0,0 +1,5 @@
+rm -rf customapi-integration docker-compose email-notifications jenkins-slave jfrog-integration job-dsl node_modules pipeline_script
+rm -rf scripts slack-notifications sonarqube ssh-agent jenkins-docker
+cp -Rv docker-demo/* .
+rm -rf docker-demo
+rm -rf README.md
