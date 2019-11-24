2f753ed3fc71de9847b2bfb83d45ed58e41efbfb
Narayanan Krishnan <narayanan.k.1985@gmail.com>
2019-11-24 16:26:36 +0000

    Modified job-dsl/nodejs.groovy script

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -13,21 +13,6 @@ job('NodeJS_DSL_example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("rm -rf customapi-integration")
-		shell("rm -rf docker-compose")
-		shell("rm -rf email-notifications")
-		shell("rm -rf jenkins-slave")
-		shell("rm -rf jfrog-integration")
-		shell("rm -rf job-dsl")
-		shell("rm -rf node_modules")
-		shell("rm -rf pipeline_script")
-		shell("rm -rf scripts")
-		shell("rm -rf slack-notifications")
-		shell("rm -rf sonarqube")
-		shell("rm -rf ssh-agent")
-		shell("rm -rf jenkins-docker")
-		shell("cp -Rv docker-demo/* .")
-		shell("rm -rf docker-demo")
-        shell("npm install")
+        shell(readFileFromWorkspace('build.sh'))
     }
 }
