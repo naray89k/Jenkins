77a9e5a99740c223e56e4284b09fe2b73f9f629d
Narayanan Krishnan <narayanan.k.1985@gmail.com>
2019-11-24 16:12:48 +0000

    Modified job-dsl/nodejs.groovy script

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
-job('NodeJS example') {
+job('NodeJS_DSL_example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/naray89k/Jenkins.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('naray89k')
+            node / gitConfigEmail('narayanan.k.1985@gmail.com')
         }
     }
     triggers {
@@ -13,6 +13,21 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
+        shell("rm -rf customapi-integration)
+		shell("rm -rf docker-compose")
+		shell("rm -rf email-notifications")
+		shell("rm -rf jenkins-slave")
+		shell("rm -rf jfrog-integration")
+		shell("rm -rf job-dsl")
+		shell("rm -rf node_modules")
+		shell("rm -rf pipeline_script")
+		shell("rm -rf scripts")
+		shell("rm -rf slack-notifications")
+		shell("rm -rf sonarqube")
+		shell("rm -rf ssh-agent")
+		shell("rm -rf jenkins-docker")
+		shell("cp -Rv docker-demo/* .")
+		shell("rm -rf docker-demo")
         shell("npm install")
     }
 }
