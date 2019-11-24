22ec96a0a8c2cf58112fe558588e09f5de82a8a6
Narayanan Krishnan <narayanan.k.1985@gmail.com>
2019-11-24 18:29:47 +0000

    updated job-dsl/nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,20 +1,20 @@
-job('NodeJS Docker example') {
+job('NodeJS_DSL_example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/naray89k/Jenkins.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('naray89k')
+            node / gitConfigEmail('narayanan.k.1985@gmail.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('nodejs') // this is the name of the NodeJS installation in
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('naray89k/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
