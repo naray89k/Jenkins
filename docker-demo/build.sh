a35ef89ef85e562ec7baefa61b4d74262781098f
Narayanan Krishnan <narayanan.k.1985@gmail.com>
2019-11-24 16:46:38 +0000

    added build.sh

--- docker-demo/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-rm -rf customapi-integration docker-compose email-notifications jenkins-slave jfrog-integration job-dsl node_modules pipeline_script
-rm -rf scripts slack-notifications sonarqube ssh-agent jenkins-docker
-cp -Rv docker-demo/* .
-rm -rf docker-demo
-npm install
